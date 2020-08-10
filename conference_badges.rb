1a1a8934e9dec3ec4bbea14e1ca8f5bfa1b1f9aa
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-10 00:57:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator(attendees)
   badges = []
   attendees. each do |attendee|
-    badges.push("Hello, my name is #{name}.")
+    badges.push("Hello, my name is #{attendee}.")
   end
   return badges
 end 
