8cde24ec1bd34e90ebc925314e29245766b09858
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-10 00:07:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def badge_maker(names)
-  names.each { |name| name = "Hello, my name #{name}." }
-end 
-
+  names.each do { |name| name = "Hello, my name #{name}." }
+   end 
+end
 badge_maker(name)
